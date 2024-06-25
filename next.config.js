5e6bd86bf769f24c763c7aee8e995aecd1d24a93
Your Name <you@example.com>
2024-06-25 12:38:32 +0500

    "Updated next.config.js: commented out code, removed dev and buildId params, and added rewrites for subdomain and main domain"

--- next.config.js
+++ next.config.js
@@ -1,22 +1,57 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true, // Enable React Strict Mode
+
+//   // Define other configurations as needed
+//   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//     // Important: Add support for source maps
+//     if (!isServer) {
+//       config.devtool = 'source-map';
+//     }
+
+//     // Example: Add any additional webpack plugins or loaders
+//     // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+
+//     return config;
+//   },
+
+//   // Example: Define other Next.js configurations
+//   // e.g., basePath, assetPrefix, images, redirects, headers, etc.
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Enable React Strict Mode
 
-  // Define other configurations as needed
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config, { isServer }) => {
     // Important: Add support for source maps
     if (!isServer) {
       config.devtool = 'source-map';
     }
 
-    // Example: Add any additional webpack plugins or loaders
-    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-
     return config;
   },
 
-  // Example: Define other Next.js configurations
-  // e.g., basePath, assetPrefix, images, redirects, headers, etc.
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'softbuilt.ghulammujtaba.com',
+          },
+        ],
+        destination: '/softbuilt/:path*', // Rewrite to /softbuilt for subdomain
+      },
+      {
+        source: '/:path*', // Default source for main domain
+        destination: '/:path*', // Default rewrite for main domain
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
