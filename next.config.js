85d938bc2ec64c47d10444c3cd6e726016c7816c
Your Name <you@example.com>
2024-06-26 08:19:10 +0500

    "Refactor next.config.js, update sitemap-0.xml, modify server.js, and change vercel.json"

--- next.config.js
+++ next.config.js
@@ -1,56 +1,34 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true, // Enable React Strict Mode
-
-//   webpack: (config, { isServer }) => {
-//     // Important: Add support for source maps
-//     if (!isServer) {
-//       config.devtool = 'source-map';
-//     }
-
-//     return config;
-//   },
-
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/softbuilt/:path*',
-//         has: [
-//           {
-//             type: 'host',
-//             value: 'softbuilt.ghulammujtaba.com',
-//           },
-//         ],
-//         destination: '/:path*',
-//       },
-//       {
-//         source: '/:path*',
-//         destination: '/:path*',
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Enable React Strict Mode
 
-  // Define other configurations as needed
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config, { isServer }) => {
     // Important: Add support for source maps
     if (!isServer) {
       config.devtool = 'source-map';
     }
 
-    // Example: Add any additional webpack plugins or loaders
-    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-
     return config;
   },
 
-  // Example: Define other Next.js configurations
-  // e.g., basePath, assetPrefix, images, redirects, headers, etc.
+  async rewrites() {
+    return [
+      {
+        source: '/softbuilt/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'softbuilt.ghulammujtaba.com',
+          },
+        ],
+        destination: '/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
