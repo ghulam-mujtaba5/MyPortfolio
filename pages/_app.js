35c46fe9b06ebea2d87314ed47c0962370e71c4e
Your Name <you@example.com>
2024-06-26 14:58:41 +0500

    "Updated _app.js and index.js files with Google Analytics integration and refactored code organization"

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,58 @@
+// // export default MyApp;
+// import { Fragment } from "react";
+// import Head from "next/head";
+// import { ThemeProvider } from '../context/ThemeContext';
+// import './global.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Fragment>
+//       <Head>
+//         <title>Portfolio</title>
+//         <meta
+//           name="viewport"
+//           content="minimum-scale=1, initial-scale=1, width=device-width"
+//         />
+//       </Head>
+//       <ThemeProvider>
+//         <Component {...pageProps} />
+//       </ThemeProvider>
+//     </Fragment>
+//   );
+// }
+
 // export default MyApp;
-import { Fragment } from "react";
+
+
+// pages/_app.js
+import { Fragment, useEffect } from "react";
 import Head from "next/head";
 import { ThemeProvider } from '../context/ThemeContext';
-import './global.css'
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+import './global.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    // Log initial pageview on component mount
+    gtag.pageview(router.pathname);
+
+    // Listen for route changes and log pageviews
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Clean up listeners when component unmounts
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events, router.pathname]);
+
   return (
     <Fragment>
       <Head>
@@ -14,6 +62,21 @@ function MyApp({ Component, pageProps }) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+      <Script
+        id="google-analytics"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
