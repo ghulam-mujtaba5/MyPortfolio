36fe3950c31c19861f7b5d3a597f0b4b56b1c233
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2024-07-28 19:54:36 +0500

    Update margin-left in NavBarCommon.module.css

--- components/Icon/sbicon.js
+++ components/Icon/sbicon.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useMemo, useRef, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useTheme } from '../../context/ThemeContext';
 import Image from 'next/image';
@@ -7,21 +7,47 @@ import styles from './sbicon.module.css'; // Ensure the correct path and module 
 const ThemeToggleIcon = () => {
   const { theme } = useTheme();
   const router = useRouter();
+  const [animate, setAnimate] = useState(false);
+  const iconRef = useRef(null);
 
   const handleIconClick = useCallback(() => {
     router.push('/');
   }, [router]);
 
   const iconClass = useMemo(() => {
-    return `${styles['theme-toggle-icon']} ${theme === 'light' ? styles.light : styles.dark}`;
-  }, [theme]);
+    return `${styles['theme-toggle-icon']} ${theme === 'light' ? styles.light : styles.dark} ${animate ? styles.animate : ''}`;
+  }, [theme, animate]);
 
   const iconSrc = useMemo(() => {
-    return theme === 'light' ? "/sbVector.svg" : "/sbVector.svg";
-  }, [theme]);
+    return "/sbVector.svg"; // Assuming the same icon for both themes
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setAnimate(true);
+          // Reset animation class after animation completes
+          setTimeout(() => setAnimate(false), 1000); // Match duration with animation time
+        }
+      },
+      { threshold: 0.1 } // Adjust based on when you want the animation to trigger
+    );
+
+    if (iconRef.current) {
+      observer.observe(iconRef.current);
+    }
+
+    return () => {
+      if (iconRef.current) {
+        observer.unobserve(iconRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div
+      ref={iconRef}
       className={iconClass}
       onClick={handleIconClick}
       role="button"
