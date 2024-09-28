e6a3bbb1475b368f795bdcb061fbebaa34afdf56
Ghulam Mujtaba <ghulammujtaba1005@gmail.com>
2024-09-29 00:16:19 +0500

    Refactor: Update navigation links in NavBar component

--- components/NavBar_Desktop/nav-bar.js
+++ components/NavBar_Desktop/nav-bar.js
@@ -1,117 +1,117 @@
-// // import styles from "./nav-bar.module.css";
-// // import { useState } from "react";
-// // import { useRouter } from "next/router";
+// // // import styles from "./nav-bar.module.css";
+// // // import { useState } from "react";
+// // // import { useRouter } from "next/router";
 
-// // const NavBar = () => {
-// //   const [hover, setHover] = useState(false);
-// //   const router = useRouter();
+// // // const NavBar = () => {
+// // //   const [hover, setHover] = useState(false);
+// // //   const router = useRouter();
 
-// //   const handleNavigation = (path) => {
-// //     router.push(path);
-// //   };
+// // //   const handleNavigation = (path) => {
+// // //     router.push(path);
+// // //   };
 
-// //   const handleMouseHover = (state) => {
-// //     setHover(state);
-// //   };
+// // //   const handleMouseHover = (state) => {
+// // //     setHover(state);
+// // //   };
 
-// //   const handleScrollToSection = (sectionId) => {
-// //     const section = document.getElementById(sectionId);
-// //     if (section) {
-// //       section.scrollIntoView({ behavior: "smooth" });
-// //     }
-// //   };
+// // //   const handleScrollToSection = (sectionId) => {
+// // //     const section = document.getElementById(sectionId);
+// // //     if (section) {
+// // //       section.scrollIntoView({ behavior: "smooth" });
+// // //     }
+// // //   };
 
-// //   return (
-// //     <header className={styles.header}>
-// //       {/* Home button */}
-// //       <button
-// //         className={styles.home}
-// //         onClick={() => handleScrollToSection("home-section")}
-// //       >
-// //         <b className={styles.homeText}>Home</b>
-// //       </button>
+// // //   return (
+// // //     <header className={styles.header}>
+// // //       {/* Home button */}
+// // //       <button
+// // //         className={styles.home}
+// // //         onClick={() => handleScrollToSection("home-section")}
+// // //       >
+// // //         <b className={styles.homeText}>Home</b>
+// // //       </button>
 
-// //       {/* About section */}
-// //       <div
-// //         className={styles.about}
-// //         onClick={() => handleScrollToSection("about-section")}
-// //       >
-// //         <div className={styles.aboutText}>About</div>
-// //       </div>
+// // //       {/* About section */}
+// // //       <div
+// // //         className={styles.about}
+// // //         onClick={() => handleScrollToSection("about-section")}
+// // //       >
+// // //         <div className={styles.aboutText}>About</div>
+// // //       </div>
 
-// //       {/* Skills section */}
-// //       <div
-// //         className={styles.skills}
-// //         onClick={() => handleScrollToSection("languages-section")}
-// //       >
-// //         <div className={styles.skillsText}>Skills</div>
-// //       </div>
+// // //       {/* Skills section */}
+// // //       <div
+// // //         className={styles.skills}
+// // //         onClick={() => handleScrollToSection("languages-section")}
+// // //       >
+// // //         <div className={styles.skillsText}>Skills</div>
+// // //       </div>
 
-// //       {/* Logo and Name Animation */}
-// //       <div
-// //         className={styles.logoAnimation}
-// //         onMouseEnter={() => handleMouseHover(true)}
-// //         onMouseLeave={() => handleMouseHover(false)}
-// //         onClick={() => handleNavigation("/softbuilt")}
-// //       >
-// //         <button className={`${styles.logo} ${hover ? styles.logoHover : ""}`}>
-// //           <img
-// //             className={styles.logoIcon}
-// //             alt="Logo"
-// //             src={hover ? "sb.svg" : "gmVectorDark.svg"} // Different SVG based on hover state
-// //             style={{
-// //               width: hover ? "30px" : "40px", // Adjust width based on hover state
-// //               height: hover ? "30px" : "40px", // Adjust height based on hover state
-// //             }}
-// //           />
-// //         </button>
-// //         <div className={styles.typo}>
-// //           {!hover && (
-// //             <img
-// //               className={styles.nameIcon}
-// //               loading="lazy"
-// //               alt="Ghulam Mujtaba"
-// //               src="/ghulam-mujtaba.svg"
-// //               style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
-// //             />
-// //           )}
-// //           {hover && (
-// //             <img
-// //               className={styles.alternativeNameIcon}
-// //               loading="lazy"
-// //               alt="SoftBuilt"
-// //               src="/sbname.svg" // Alternative SVG on hover
-// //               style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
-// //             />
-// //           )}
-// //         </div>
-// //       </div>
+// // //       {/* Logo and Name Animation */}
+// // //       <div
+// // //         className={styles.logoAnimation}
+// // //         onMouseEnter={() => handleMouseHover(true)}
+// // //         onMouseLeave={() => handleMouseHover(false)}
+// // //         onClick={() => handleNavigation("/softbuilt")}
+// // //       >
+// // //         <button className={`${styles.logo} ${hover ? styles.logoHover : ""}`}>
+// // //           <img
+// // //             className={styles.logoIcon}
+// // //             alt="Logo"
+// // //             src={hover ? "sb.svg" : "gmVectorDark.svg"} // Different SVG based on hover state
+// // //             style={{
+// // //               width: hover ? "30px" : "40px", // Adjust width based on hover state
+// // //               height: hover ? "30px" : "40px", // Adjust height based on hover state
+// // //             }}
+// // //           />
+// // //         </button>
+// // //         <div className={styles.typo}>
+// // //           {!hover && (
+// // //             <img
+// // //               className={styles.nameIcon}
+// // //               loading="lazy"
+// // //               alt="Ghulam Mujtaba"
+// // //               src="/ghulam-mujtaba.svg"
+// // //               style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
+// // //             />
+// // //           )}
+// // //           {hover && (
+// // //             <img
+// // //               className={styles.alternativeNameIcon}
+// // //               loading="lazy"
+// // //               alt="SoftBuilt"
+// // //               src="/sbname.svg" // Alternative SVG on hover
+// // //               style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
+// // //             />
+// // //           )}
+// // //         </div>
+// // //       </div>
 
-// //       {/* Resume section */}
-// //       <div className={styles.resume} onClick={() => handleNavigation("/resume")}>
-// //         <div className={styles.resumeText}>Resume</div>
-// //       </div>
+// // //       {/* Resume section */}
+// // //       <div className={styles.resume} onClick={() => handleNavigation("/resume")}>
+// // //         <div className={styles.resumeText}>Resume</div>
+// // //       </div>
 
-// //       {/* Project section */}
-// //       <div
-// //         className={styles.project}
-// //         onClick={() => handleScrollToSection("project-section")}
-// //       >
-// //         <div className={styles.projectText}>Project</div>
-// //       </div>
+// // //       {/* Project section */}
+// // //       <div
+// // //         className={styles.project}
+// // //         onClick={() => handleScrollToSection("project-section")}
+// // //       >
+// // //         <div className={styles.projectText}>Project</div>
+// // //       </div>
 
-// //       {/* Contact section */}
-// //       <div
-// //         className={styles.contact}
-// //         onClick={() => handleScrollToSection("contact-section")}
-// //       >
-// //         <div className={styles.contactText}>Contact</div>
-// //       </div>
-// //     </header>
-// //   );
-// // };
+// // //       {/* Contact section */}
+// // //       <div
+// // //         className={styles.contact}
+// // //         onClick={() => handleScrollToSection("contact-section")}
+// // //       >
+// // //         <div className={styles.contactText}>Contact</div>
+// // //       </div>
+// // //     </header>
+// // //   );
+// // // };
 
-// // export default NavBar;
+// // // export default NavBar;
 
 
 // import styles from "./nav-bar.module.css";
@@ -204,6 +204,12 @@
 //         <div className={styles.resumeText}>Resume</div>
 //       </div>
 
+//       {/* Resume section */}
+//       {/* <Link href="/resume" passHref>
+//         <div className={styles.resume}>
+//           <div className={styles.resumeText}>Resume</div>
+//         </div>
+//       </Link> */}
 //       {/* Project section */}
 //       <div
 //         className={styles.project}
@@ -226,10 +232,11 @@
 // export default NavBar;
 import styles from "./nav-bar.module.css";
 import { useState } from "react";
-import Link from 'next/link'; // Import Link from Next.js
+import { useRouter } from "next/router"; // Import useRouter from Next.js
 
 const NavBar = () => {
   const [hover, setHover] = useState(false);
+  const router = useRouter(); // Initialize the router
 
   const handleMouseHover = (state) => {
     setHover(state);
@@ -240,6 +247,10 @@ const NavBar = () => {
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
+  };
+
+  const handleNavigation = (path) => {
+    router.push(path); // Use router to navigate to the specified path
   };
 
   return (
@@ -269,52 +280,49 @@ const NavBar = () => {
       </div>
 
       {/* Logo and Name Animation */}
-      <Link href="http://softbuilt.ghulammujtaba.com" passHref>
-        <div
-          className={styles.logoAnimation}
-          onMouseEnter={() => handleMouseHover(true)}
-          onMouseLeave={() => handleMouseHover(false)}
-        >
-          <button className={`${styles.logo} ${hover ? styles.logoHover : ""}`}>
+      <div
+        className={styles.logoAnimation}
+        onMouseEnter={() => handleMouseHover(true)}
+        onMouseLeave={() => handleMouseHover(false)}
+        onClick={() => handleNavigation("/softbuilt")} // Navigate on click
+      >
+        <button className={`${styles.logo} ${hover ? styles.logoHover : ""}`}>
+          <img
+            className={styles.logoIcon}
+            alt="Logo"
+            src={hover ? "sb.svg" : "gmVectorDark.svg"} // Different SVG based on hover state
+            style={{
+              width: hover ? "30px" : "40px", // Adjust width based on hover state
+              height: hover ? "30px" : "40px", // Adjust height based on hover state
+            }}
+          />
+        </button>
+        <div className={styles.typo}>
+          {!hover && (
             <img
-              className={styles.logoIcon}
-              alt="Logo"
-              src={hover ? "sb.svg" : "gmVectorDark.svg"} // Different SVG based on hover state
-              style={{
-                width: hover ? "30px" : "40px", // Adjust width based on hover state
-                height: hover ? "30px" : "40px", // Adjust height based on hover state
-              }}
+              className={styles.nameIcon}
+              loading="lazy"
+              alt="Ghulam Mujtaba"
+              src="/ghulam-mujtaba.svg"
+              style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
             />
-          </button>
-          <div className={styles.typo}>
-            {!hover && (
-              <img
-                className={styles.nameIcon}
-                loading="lazy"
-                alt="Ghulam Mujtaba"
-                src="/ghulam-mujtaba.svg"
-                style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
-              />
-            )}
-            {hover && (
-              <img
-                className={styles.alternativeNameIcon}
-                loading="lazy"
-                alt="SoftBuilt"
-                src="/sbname.svg" // Alternative SVG on hover
-                style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
-              />
-            )}
-          </div>
+          )}
+          {hover && (
+            <img
+              className={styles.alternativeNameIcon}
+              loading="lazy"
+              alt="SoftBuilt"
+              src="/sbname.svg" // Alternative SVG on hover
+              style={{ maxWidth: "100%", height: "auto" }} // Make the image responsive
+            />
+          )}
         </div>
-      </Link>
+      </div>
 
       {/* Resume section */}
-      <Link href="/resume" passHref>
-        <div className={styles.resume}>
-          <div className={styles.resumeText}>Resume</div>
-        </div>
-      </Link>
+      <div className={styles.resume} onClick={() => handleNavigation("/resume")}>
+        <div className={styles.resumeText}>Resume</div>
+      </div>
 
       {/* Project section */}
       <div
