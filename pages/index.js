45a45d19b28824a24cc3a862c47f5ded022b0a10
Ghulam Mujtaba <ghulammujtaba1005@gmail.com>
2024-09-28 21:18:35 +0500

    Refactor router push in Home component

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
 
-const Home = () => {
-    const router = useRouter();
+// const Home = () => {
+//     const router = useRouter();
 
-    useEffect(() => {
-        // Redirects to '/portfolio'
-        router.push('/portfolio');
-    }, [router]);
+//     useEffect(() => {
+//         // Redirects to '/portfolio'
+//         router.push('/portfolio');
+//     }, [router]);
 
-    return null; // No need to render anything
-};
+//     return null; // No need to render anything
+// };
 
-export default Home;
+// export default Home;
