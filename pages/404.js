262b1f01bd2b92d85f2541148d67fe6e062c9394
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-05-17 20:13:59 +0500

    Refactor: Fix logo source path in custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -16,7 +16,7 @@ export default function Custom404() {
         transition={{ duration: 0.8, ease: 'easeOut' }}
       >
         <Image
-          src={theme === 'dark' ? '/gmVectorDark.svg' : '\gmVector.svg'}
+          src={theme === 'dark' ? '/gmVectorDark.svg' : '/gmVector.svg'}
           alt="Logo"
           width={80}
           height={80}
