0b99295116408bae1a838968ff35b1637e3ba8cf
ghulam-mujtaba5 <ghulammujtaba1005@gmail.com>
2025-06-22 15:52:54 +0500

    feat: add cookie emoji to CookieConsentBanner for enhanced visual appeal

--- components/CookieConsentBanner/CookieConsentBanner.js
+++ components/CookieConsentBanner/CookieConsentBanner.js
@@ -58,6 +58,7 @@ const CookieConsentBanner = () => {
         >
           Learn more
         </a>.
+        <span style={{ fontSize: '1.3em', marginLeft: '0.4em', verticalAlign: 'middle' }}>🍪</span>
       </span>
       <div className={`${commonStyles.actions} ${frameStyles.actions}`}>
         <button
